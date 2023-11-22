3f2a22e5c0fcdc943912b523bab49d16b895ca66
adhaozy <adhaozy81@gmail.com>
2023-11-23 06:33:00 +0700

    hari 8

---
+++ Example/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program{
+    public static void Main()
+    {
+        Action<bool> x;
+        Action<bool, bool, int, int, bool> y;
+        Func<int, int , bool> comparer = Comparison;
+        comparer += (int a, int b) => a => b;
+        global::System.Object value = Console.WriteLine(comparer.Invoke(3,3));
+    }
+}
+bool Comparison(int a, int b) => a == b;
+
+int Add(int a, int b)
+{
+    return a + b;
+}
