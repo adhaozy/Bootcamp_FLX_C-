3eff6c7b09de5cb51c0818f8789e874d2e0044b2
adhaozy <adhaozy81@gmail.com>
2023-11-23 11:52:06 +0700

    hari9

---
+++ List/Program.cs
@@ -0,0 +1,42 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        List<int> myInt = new();
+        myInt.Add(3);
+        myInt.Add(4);
+        myInt.Add(5);
+
+        int result = myInt[0];
+        // Console.WriteLine(result);
+
+        List<int> myInt2 = new() {1,2,3,4,5};
+        myInt2.Add(6);
+        Console.WriteLine(myInt2[5]);
+
+        List<Player> players = new()
+        {
+            new Player(1, "Markenji"),
+            new Player(2, "Markenji"),
+            new Player(3, "Markenji")
+        };
+
+        Console.WriteLine($"{players[1].Id}, {players[1].Name}");
+        Console.WriteLine(players[0]);
+    }
+}
+
+class Player{
+    public int Id{get;}
+    public string Name{get;}
+    public Player(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+    public override string ToString()
+    {
+        return $"{Id}, {Name}";
+    }
+}
---
+++ TryCatchFinally/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+class Program{
+    static void Main()
+    {
+        int a = 10;
+        int b = 0;
+        try{
+            int result = a / b;
+
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine($"{e.Message}");
+        }
+        finally
+        {
+            // Release umanaged resource
+            Console.WriteLine("Finally");
+        }
+        Console.WriteLine("Program finished");
+    }
+}
