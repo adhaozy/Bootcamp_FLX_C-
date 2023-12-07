2e332029daa32f01623d2ed2ce2312043c5c7fa1
adhaozy <adhaozy81@gmail.com>
2023-12-07 11:52:56 +0700

    day13

---
+++ A_ConditionalCOmpilation/Program.cs
@@ -0,0 +1,21 @@
+﻿//For choose between different Testing or Lib
+
+//dotnet build -C GAMERUNNER
+//dotnet run
+
+class Program {
+	static void Main() {
+		#if CODE
+		#elif RELEASE 
+		Console.WriteLine("GameRunner.");
+		//GameRunner gameRunner = new();
+		#elif (TEST || DEBUG)
+		Console.WriteLine("GameTester.");
+		//GameTester gameTest = new();
+		//methodTest(0);
+		#else
+		Console.WriteLine("Not anything.");
+		#endif
+		Console.WriteLine("Finish");
+	}
+}
---
+++ B_ConditionalCompilation2/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program{
+    static void Main()
+    {
+        #if ANDROID
+        Console.WriteLine("ANDROID");
+        #elif APPLE
+        Console.WriteLine("Apple");
+        #endif
+        Console.WriteLine("Program berjalan...");
+    }
+}
