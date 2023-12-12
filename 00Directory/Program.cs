3028351fc242d62c901cbb7c6a691c76499b3cb0
adhaozy <adhaozy81@gmail.com>
2023-12-12 11:36:10 +0700

    day16

---
+++ 00Directory/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+class Program {
+	static void Main() {
+		string path = @"C:\Users\Batch 7\Documents\Ahmadi\";
+
+		Directory.CreateDirectory(path);
+
+		if (Directory.Exists(path)) {
+			Console.WriteLine("Directory exists.");
+		}
+
+		// Create a file in the directory
+		string filePath = Path.Combine(path, "bootcamp.txt");
+		File.WriteAllText(filePath, "AIM YANG BENER MAD");
+
+		string[] files = Directory.GetFiles(path);
+		foreach (string file in files) {
+			Console.WriteLine(file);
+		}
+
+		Directory.Delete(path, true);
+	}
+}
