e5b5e29f841c1d267fecbf5ca0f33394ef7b5c9d
adhaozy <adha.dewangga@m3.formulatrix.com>
2023-12-29 11:32:15 +0700

    day21

---
+++ NoDesainPattern/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace DoFactory.GangOfFour.Factory.Structural
+{
+    class MainApp
+    {
+        static void Main()
+        {
+            // Create products directly without using Factory Method
+
+            ConcreteProductA productA = new ConcreteProductA();
+            Console.WriteLine("Created {0}", productA.GetType().Name);
+
+            ConcreteProductB productB = new ConcreteProductB();
+            Console.WriteLine("Created {0}", productB.GetType().Name);
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+
+    // The 'Product' abstract class
+    abstract class Product
+    {
+    }
+
+    // A 'ConcreteProduct' class
+    class ConcreteProductA : Product
+    {
+    }
+
+    // A 'ConcreteProduct' class
+    class ConcreteProductB : Product
+    {
+    }
+
+    // The 'Creator' abstract class
+    abstract class Creator
+    {
+        public abstract Product FactoryMethod();
+    }
+
+    // A 'ConcreteCreator' class
+    class ConcreteCreatorA : Creator
+    {
+        public override Product FactoryMethod()
+        {
+            return new ConcreteProductA();
+        }
+    }
+
+    // A 'ConcreteCreator' class
+    class ConcreteCreatorB : Creator
+    {
+        public override Product FactoryMethod()
+        {
+            return new ConcreteProductB();
+        }
+    }
+}
